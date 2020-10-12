27d604d82bd81eaf019ca0daee834a1cee9e64af
Walter Kieke <walterkieke@Walters-MacBook-Pro.local>
2020-10-12 03:33:43 -0500

    upload

---
+++ build/precache-manifest.1fe0088146574b7db29ef3b880a577eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5cb195271a9d3b6708ed9223268688",
+    "url": "/R-Portfolio/index.html"
+  },
+  {
+    "revision": "d734d995e60477abf47a",
+    "url": "/R-Portfolio/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "e7d2e3b438069c12a596",
+    "url": "/R-Portfolio/static/css/main.d3b8b9cd.chunk.css"
+  },
+  {
+    "revision": "d734d995e60477abf47a",
+    "url": "/R-Portfolio/static/js/2.216d71ff.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/R-Portfolio/static/js/2.216d71ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d2e3b438069c12a596",
+    "url": "/R-Portfolio/static/js/main.4966e7f1.chunk.js"
+  },
+  {
+    "revision": "571e89dccbf81d10d1e0",
+    "url": "/R-Portfolio/static/js/runtime-main.095152a3.js"
+  },
+  {
+    "revision": "7b14a867d3eec15734635d89ca8c30fd",
+    "url": "/R-Portfolio/static/media/Walter.7b14a867.pdf"
+  },
+  {
+    "revision": "9d8c1c28455e8ddae47e5d75458b5163",
+    "url": "/R-Portfolio/static/media/profile.9d8c1c28.JPG"
+  }
+]);
